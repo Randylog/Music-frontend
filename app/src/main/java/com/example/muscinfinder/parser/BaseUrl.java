41ee467bb74ceaa7900ea2eb5598a48967fa93f5
RandyLog <tatsuyasusan@gmail.com>
2020-08-29 17:54:31 +0545

    my commit

---
+++ app/src/main/java/com/example/muscinfinder/parser/BaseUrl.java
@@ -0,0 +1,17 @@
+package com.example.muscinfinder.parser;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+//using baseurl to run in android app, adding tokens
+public class BaseUrl {
+    public static final String base_url="http://10.0.2.2:5000/";
+    public static String token = "Bearer ";
+    public static Retrofit getInstance() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
